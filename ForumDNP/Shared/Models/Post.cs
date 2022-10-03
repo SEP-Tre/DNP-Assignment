db1733a39d31f4b2f478970ec122fb6208a1eed7
KamilFischbach <92295061+KamilFischbach@users.noreply.github.com>
2022-10-03 13:59:43 +0200

    Domain model and following model classes.

---
+++ ForumDNP/Shared/Models/Post.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shared;
+
+public class Post
+{
+    public User owner { get; set; }
+    public string title { get; set; }
+    public string body { get; set; }
+    public DateTime DateTime { get; set; }
+
+    public Post(User owner, string title)
+    {
+        this.owner = owner;
+        this.title = title;
+    }
+}
