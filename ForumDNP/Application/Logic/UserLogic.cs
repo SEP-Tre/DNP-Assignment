213e26e9d942cdbb452988d49f886e57afbccb63
KamilFischbach <92295061+KamilFischbach@users.noreply.github.com>
2022-10-03 15:31:37 +0200

    Creation of Users working and slightly tested.

---
+++ ForumDNP/Application/Logic/UserLogic.cs
@@ -0,0 +1,64 @@
+﻿using Application.DaoInterfaces;
+using Application.LogicInterfaces;
+using Shared;
+using Shared.DTOs;
+
+namespace Application.Logic;
+
+public class UserLogic : IUserLogic
+{
+    private readonly IUserDao userDao;
+
+    public UserLogic(IUserDao userDao)
+    {
+        this.userDao = userDao;
+    }
+
+    public async Task<User> CreateAsync(UserCreationDto userToCreate)
+    {
+        User? exsisting = await userDao.GetByUsernameAsync(userToCreate.UserName);
+
+        if (exsisting != null)
+        {
+            throw new Exception("Such user already exists.");
+        }
+        ValidateData(userToCreate);
+        User newUser = new User()
+        {
+            UserName = userToCreate.UserName,
+            Password = userToCreate.Password
+        };
+
+        User created = await userDao.CreateAsync(newUser);
+
+        return created;
+    }
+    
+
+    private static void ValidateData(UserCreationDto dto)
+    {
+        string username = dto.UserName;
+        string password = dto.Password;
+
+        if (String.IsNullOrEmpty(username) || String.IsNullOrEmpty(password))
+        {
+            throw new Exception("Username or password cannot be empty.");
+        }
+
+        if (username.Length <= 6)
+        {
+            throw new Exception("Username has to have more than 6 characters.");
+        }
+
+        if (password.ToLower().Contains(username.ToLower()))
+        {
+            throw new Exception("Password and username cannot be the same");
+        }
+
+        if (password.Length <= 6)
+        {
+            throw new Exception("Password has to have more than 6 characters.");
+        }
+    }
+
+}
--- ForumDNP/Shared/Models/Post.cs
+++ ForumDNP/Shared/Models/Post.cs
@@ -4,14 +4,15 @@ namespace Shared;
 
 public class Post
 {
-    public User owner { get; set; }
-    public string title { get; set; }
-    public string body { get; set; }
+    public int Id { get; set; }
+    public User Owner { get; set; }
+    public string Title { get; set; }
+    public string Body { get; set; }
     public DateTime DateTime { get; set; }
 
     public Post(User owner, string title)
     {
-        this.owner = owner;
-        this.title = title;
+        this.Owner = owner;
+        this.Title = title;
     }
 }
