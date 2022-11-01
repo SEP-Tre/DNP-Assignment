a1cb9144a256727c0a655ab97d989c85e6b0bd44
Kamil <fischbach.kamil@gmail.com>
2022-11-01 16:05:02 +0100

    blazor part

---
+++ ForumDNP/HttpClients/ClientInterfaces/IPostService.cs
@@ -0,0 +1,9 @@
+﻿using Shared;
+using Shared.DTOs;
+
+namespace HttpClients.ClientInterfaces;
+
+public interface IPostService
+{
+    Task CreateAsync(PostCreationDto dto);
+}
---
+++ ForumDNP/HttpClients/Implementations/PostHttpClient.cs
@@ -0,0 +1,26 @@
+﻿using System.Net.Http.Json;
+using HttpClients.ClientInterfaces;
+using Shared;
+using Shared.DTOs;
+
+namespace HttpClients.Implementations;
+
+public class PostHttpClient : IPostService
+{
+    private readonly HttpClient client;
+
+    public PostHttpClient(HttpClient client)
+    {
+        this.client = client;
+    }
+    
+    public async Task CreateAsync(PostCreationDto dto)
+    {
+        HttpResponseMessage response = await client.PostAsJsonAsync("/posts", dto);
+        if (!response.IsSuccessStatusCode)
+        {
+            string result = await response.Content.ReadAsStringAsync();
+            throw new Exception(result);
+        }   
+    }
+}
