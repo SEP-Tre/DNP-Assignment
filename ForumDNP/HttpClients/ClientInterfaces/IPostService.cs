3b0f00b24311985cb547423c1fe9f20d54a58cd9
Kamil <fischbach.kamil@gmail.com>
2022-11-03 11:33:22 +0100

    View post page created

--- ForumDNP/HttpClients/ClientInterfaces/IPostService.cs
+++ ForumDNP/HttpClients/ClientInterfaces/IPostService.cs
@@ -7,4 +7,5 @@ public interface IPostService
 {
     Task CreateAsync(PostCreationDto dto);
     Task<ICollection<string>> GetTitlesAsync();
+    Task<Post> GetByTitleAsync(string title);
 }
--- ForumDNP/HttpClients/Implementations/PostHttpClient.cs
+++ ForumDNP/HttpClients/Implementations/PostHttpClient.cs
@@ -14,7 +14,7 @@ public class PostHttpClient : IPostService
     {
         this.client = client;
     }
-    
+
     public async Task CreateAsync(PostCreationDto dto)
     {
         HttpResponseMessage response = await client.PostAsJsonAsync("/posts", dto);
@@ -22,7 +22,7 @@ public class PostHttpClient : IPostService
         {
             string result = await response.Content.ReadAsStringAsync();
             throw new Exception(result);
-        }   
+        }
     }
 
     public async Task<ICollection<string>> GetTitlesAsync()
@@ -31,13 +31,42 @@ public class PostHttpClient : IPostService
         string result = await response.Content.ReadAsStringAsync();
         if (!response.IsSuccessStatusCode)
         {
-            
             throw new Exception(result);
         }
+
         ICollection<string> titles = JsonSerializer.Deserialize<ICollection<string>>(result, new JsonSerializerOptions
         {
             PropertyNameCaseInsensitive = true
         })!;
         return titles;
+    }
+
+    public async Task<Post> GetByTitleAsync(string title)
+    {
+        string query = ConstructQuery(title);
+        HttpResponseMessage response = await
+            client.GetAsync("/posts" + query);
+        string result = await response.Content.ReadAsStringAsync();
+        if (!response.IsSuccessStatusCode)
+        {
+            throw new Exception(result);
+        }
+        Post post = JsonSerializer.Deserialize<Post>(result, new JsonSerializerOptions
+        {
+            PropertyNameCaseInsensitive = true
+        })!;
+        return post;
+    }
+
+    private static string ConstructQuery
+        (string title)
+    {
+        string query = "";
+        if (!string.IsNullOrEmpty(title))
+        {
+            query += $"?title={title}";
+        }
+
+        return query;
     }
 }
